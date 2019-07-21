7bae8f4acd25fd296f148fe24b9782f172d16e95
charlescortez <charlesmph@gmail.com>
2019-07-20 20:52:42 -0400

    Create simple UI for user to interact with

--- roast.js
+++ roast.js
@@ -1,3 +1,5 @@
+console.log('Hello?')
+
 var bodyParts = ["ears", "elbows", "eyeball", "booty", "quad", "toe", "lower back", "face", "weenis", "car", "house", "shoes", "girlfriend", "boyfriend", "nostrils", "cooking", "clothes", "lips", "hair", "soul", "cousin", "complexion", "twin", "hands"];
 var descriptions = ["thicc", "clean", "sleepy", "hungry", "fat", "shiny", "thirsty", "beautiful", "dry", "runny", "derpy", "dumb", "ugly", "pleasant", "clingy", "dirty", "flappy", "cheesy", "cute", "funny", "creepy", "hot", "cold", "painful", "weird", "old"];
 var comparisons = ["cow", "truck", "chihuahua", "donkey", "bear", "beast", "banana", "tree trunk", "pterydactyl", "Thanos", "wight walker", "spiderman", "pooper scooper", "creeper", "chick", "boi", "plant", "human", "rhino", "snek", "zombie", "plague", "toilet", "clam", "bird", "snack", "thot"];
@@ -11,3 +13,19 @@ comparisons.length)];
 
 var adlibs = ["Bruh.", "Your", bodyParts, "looks", "like", "a", descriptions, comparisons + "!!"].join(" ");
 adlibs;
+
+
+
+
+
+function run(e) {
+    const changeDiv = document.getElementsByClassName('roast');
+
+    changeDiv.innerHTML = adlibs;
+}
+
+const start = document.querySelector(`[name = "ouch"]`);
+
+// start.addEventListener('click', run);
+
+start.addEventListener('click', run);
